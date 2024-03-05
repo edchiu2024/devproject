3814170054cec64b666a004a10612428e442b65a
Ed Chiu <tungwu.chiu@gmail.com>
2024-03-05 09:23:54 -0800

    update reddit feed json file

--- src/components/data_extract.py
+++ src/components/data_extract.py
@@ -173,16 +173,9 @@ class DataExtraction:
                 flattened.extend(self._flatten_comments(comment['replies']))  # Recursively process replies
         return flattened   
       
-#if __name__=="__main__":
+if __name__=="__main__":
      
 
-    #obj=DataExtraction()
-    #print(obj.post_data_extract())
+    obj=DataExtraction()
+    print(obj.post_data_extract())
 
-    #train_data,test_data=obj.initiate_data_ingestion()
-
-    #data_transformation=DataTransformation()
-    #train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
-
-    #modeltrainer=ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
